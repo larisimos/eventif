437db1e7ec87b4b8fe4534ae3f51f4ca231ee1cb
Larissa Simões <101749641+larisimos@users.noreply.github.com>
2024-11-29 21:53:45 +0000

    formatação

--- eventif-master/contact/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    name = forms.CharField(label='Nome')
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
-    message = forms.CharField(label='Mensagem',widget=forms.Textarea)
